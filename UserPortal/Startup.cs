999b9cdd122a65568c70e0d41208e794131b9ea7
Luitzen Hietkamp <luitzenhietkamp@gmail.com>
2019-06-12 16:45:47 +0100

    change error page

--- UserPortal/Startup.cs
+++ UserPortal/Startup.cs
@@ -45,15 +45,17 @@ namespace UserPortal
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
+            // if (env.IsDevelopment())
+            // {
+                // app.UseDeveloperExceptionPage();
+            // }
+            // else
+            // {
+                // app.UseExceptionHandler("/Error");
+                // app.UseHsts();
+            // }
+			app.UseDeveloperExceptionPage();
+			
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
