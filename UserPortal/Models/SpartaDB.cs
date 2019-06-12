7dea0beadf4c6232b84c89fab94c34e782d8362f
Kirt Morgan <kirtmorgan@live.com>
2019-06-12 15:58:12 +0100

    updated startup file

--- UserPortal/Models/SpartaDB.cs
+++ UserPortal/Models/SpartaDB.cs
@@ -18,7 +18,7 @@ namespace UserPortal.Models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "SpartaDB.db");
-            optionsBuilder.UseSqlServer(@"Server=tcp:spartaportal.database.windows.net,1433;Initial Catalog=Northwind;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            optionsBuilder.UseSqlServer(@"Server=tcp:spartaportal.database.windows.net,1433;Initial Catalog=SpartaDB;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
