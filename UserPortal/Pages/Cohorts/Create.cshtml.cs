8b0690f0c06e20178246fcf864b75e6671c61374
SebastianTrifa <seb.trifa@gmail.com>
2019-06-13 15:16:14 +0100

    revert2

--- UserPortal/Pages/Cohorts/Create.cshtml.cs
+++ UserPortal/Pages/Cohorts/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -29,6 +30,8 @@ namespace UserPortal.Pages.Cohorts
 
         public async Task<IActionResult> OnPostAsync()
         {
+            if (HttpContext.Session.GetString("Test") != "1")
+                return RedirectToPage("/Login");
             if (!ModelState.IsValid)
             {
                 return Page();
